9b1bc148ea34134e256b5ba0c6282042cc35eb74
fgoebels <florian.goebels@gmail.com>
2017-06-22 10:13:36 -0400

    added go annotation file

--- Pitype2.py
+++ Pitype2.py
@@ -1,21 +1,40 @@
 from __future__ import division
 
 import numpy as np
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import precision_recall_curve, roc_curve, average_precision_score, roc_auc_score, precision_recall_fscore_support
-from sklearn.model_selection import  cross_val_predict
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.pipeline import Pipeline
-from sklearn.feature_selection import RFECV
-
-from sklearn import svm
-from sklearn import metrics
-
-# The following imported libraries are for intergrating GeneMANIA data as Functional Evidence
+import inspect
 import sys
 import urllib2
 import os
 import re
+
+
+subfldr = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"TCSS")))
+sys.path.append(subfldr)
+
+from main import load_semantic_similarity, calculate_semantic_similarity
+
+
+class GOSim(object):
+	objs = ""
+
+	def __init__(self, onto_F, gene_anno_F):
+		self.name = ["Sim_CC", "Sim_BP", "Sim_MF"]
+		if GOSim.objs == "":
+			GOSim.objs = load_semantic_similarity(onto_F, gene_anno_F, "C:2.4,P:3.5,F:3.3", "IEA")
+
+	def getScores(self, a, b, elutionData):
+		return (a, b)
+
+	def calculateScore(self, a, b):
+		out = []
+		domain_def = {'C': 'Cellular Component', 'P': 'Biological Process', 'F': 'Molecular Function'}
+		for domain in domain_def:
+			score = GOSim.objs[domain]._semantic_similarity(a, b)[0]
+			if score is None: score = 0
+			out.append(score)
+		return out
+
 
 class ELM:
 	def __init__(self):
@@ -63,14 +82,31 @@ class ELM:
 		return self.prot2counts[protA], self.prot2counts[protB]
 
 class Pitype:
-	def __init__(self):
+
+	def __init__(self, fs):
+
+		self.clf = CLF_Wrapper()
+		self.fs = self.get_fs_comb(fs)
 		return None
+
+	def get_fs_comb(self, comb_string):
+		# Create feature combination
+		scores = [ELM()]
+		this_scores = []
+		for i, feature_selection in enumerate(comb_string):
+			if feature_selection == "1": this_scores.append(scores[i])
+		return this_scores
+
+
+
+
 
 class CLF_Wrapper:
 	def __init__(self):
 		self.clf = RandomForestClassifier(n_estimators=100)
 		self.fit()
 
+	# Get reference data from online repository
 	def fit(self, data_loc = "https://raw.githubusercontent.com/fgoebels/PiType2.0/master/ob_nob.dat.txt"):
 		data_FH = urllib2.urlopen(data_loc)
 		data_FH.readline()
@@ -100,13 +136,16 @@ class CLF_Wrapper:
 		return preds
 
 def main():
-	this_ELM = ELM()
-	this_clf =  CLF_Wrapper()
-	for edge in ["P04637\tP02340", "Q2XVY7\tQ29537"]:
-		scores = np.array(this_ELM.getscore(edge)).reshape(1, -1)
-		print this_clf.predict(scores)
-		print this_clf.predict_proba(scores)
-	return None
+	mode, infile = sys.argv[1:]
+	if mode == "-test":
+		this_ELM = ELM()
+		this_clf =  CLF_Wrapper()
+		for edge in ["P04637\tP02340", "Q2XVY7\tQ29537"]:
+			scores = np.array(this_ELM.getscore(edge)).reshape(1, -1)
+			print this_clf.predict(scores)
+			print this_clf.predict_proba(scores)
+		return None
+
 
 if __name__ == "__main__":
 	try:
